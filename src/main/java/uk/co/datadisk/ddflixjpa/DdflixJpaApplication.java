37f8fc1ac92bc39e4fcd0ee9a992fe88e559727c
datadiskpfv <paul.valle@datadisk.co.uk>
2018-06-09 18:53:56 +0100

    Initial commit

---
+++ src/main/java/uk/co/datadisk/ddflixjpa/DdflixJpaApplication.java
@@ -0,0 +1,12 @@
+package uk.co.datadisk.ddflixjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdflixJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdflixJpaApplication.class, args);
+    }
+}
