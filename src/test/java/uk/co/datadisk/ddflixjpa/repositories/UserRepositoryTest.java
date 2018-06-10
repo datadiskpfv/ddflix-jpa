7f54e65e4982193f816e542f7a89019ae7705bc7
Paul Valle <paul.valle@datadisk.co.uk>
2018-06-10 15:58:47 +0100

    create a test database that the test use

--- src/test/java/uk/co/datadisk/ddflixjpa/repositories/UserRepositoryTest.java
+++ src/test/java/uk/co/datadisk/ddflixjpa/repositories/UserRepositoryTest.java
@@ -8,22 +8,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import uk.co.datadisk.ddflixjpa.DdflixJpaApplication;
 import uk.co.datadisk.ddflixjpa.entities.User;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DdflixJpaApplication.class)
+@ActiveProfiles("test")
+@TestPropertySource("classpath:application-test.properties")
 public class UserRepositoryTest {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @Autowired
-    EntityManager em;
 
     @Autowired
     UserRepository userRepository;
@@ -31,17 +31,15 @@ public class UserRepositoryTest {
     @Before
     public void setUp() {
         User user1 = User.builder().email("paul.valle@example.com").build();
-        userRepository.saveAndFlush(user1);
+        userRepository.save(user1);
     }
 
     @Test
     @Transactional
     @Rollback(false)
     public void createUser() {
-        //User user1 = User.builder().email("paul.valle@example.com").build();
         User user2 = User.builder().email("lorraine.valle@example.com").build();
-        //userRepository.saveAndFlush(user1);
-        userRepository.saveAndFlush(user2);
+        userRepository.save(user2);
 
         List<User> users = userRepository.findAll();
         logger.info("User list:" + users.toString());
